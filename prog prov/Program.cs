1e52e0b0f418888ea5e0e589a3fa5ca40d51aaa0
Elliot <elliot.widell@elev.ga.ntig.se>
2020-10-05 09:33:17 +0200

    andra commit

--- prog prov/Program.cs
+++ prog prov/Program.cs
@@ -7,14 +7,47 @@ namespace prog_prov
         static void Main(string[] args)
         {
             int rättsvar = 4;
+            string spelargissning = "";
+            int gissning = 0;
 
-            while (rättsvar != 4)
+            while (gissning != 4)
             {
+                Console.WriteLine("Du ska sänka fiende skäppet, gissa en siffra mellan 1-10!");
+                spelargissning = Console.ReadLine();
 
 
+                try
+                {
+                    gissning = Int32.Parse(spelargissning);
+
+                }
+                catch
+                {
+                    Console.WriteLine("Du skrev inte en siffra mellan 1-10");
+
+                }
+
+
+                if (gissning >= 1 && gissning <= 10)
+                {
+                    if (gissning == rättsvar - +2 || gissning == rättsvar + -1)
+                    { Console.WriteLine("Near Miss!"); }
+                    else
+                    {
+                        Console.WriteLine("Miss!");
+                    }
+
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Du skrev inte en siffra mellan 1-10");
+                }
 
             }
 
+            Console.WriteLine("Du träffade!!!");
 
 
 
